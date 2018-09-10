9d5876e0be0acc4a1e1be693235da85fe9d810ec
Mark Carter <alt.mcarter@gmail.com>
2018-09-10 19:06:06 +0100

    Parser cleanup

--- ForthIntel/parser.pas
+++ ForthIntel/parser.pas
@@ -17,12 +17,10 @@ uses
 type
 	TTokenType = (Eof, Unknown,  Identifier, Str, UInt);
 
-        TVariantList  = specialize TFPGList<Variant>;
+        //TVariantList  = specialize TFPGList<Variant>;
 	TGluteProc = procedure();
-        //TGluteFunc = function(vs:TVariantList):variant;
 
 	TProcMap = specialize TFPGMap<string, TGluteProc>;
-	//TProcMap = specialize TFPGMap<string, TGluteFunc>;
 
 
 var
@@ -31,150 +29,37 @@ var
 	yytype:TTokenType;
 	yytext:string;
 	procMap:TProcMap;
-        //IntStack:TStack;
-        //IntStack:TOrderedList;
-        //IntStack:TList;
         IntStack: array[1..200] of Integer;
         IntStackSize:Integer;
 
 
 
-procedure InitLexer(s:String);
-function yylex(var the_yytext:String):TTokenType;
+//procedure InitLexer(s:String);
+//function yylex(var the_yytext:String):TTokenType;
 procedure GluteRepl();
 procedure AddGluteProc(name:string; ptr:TGluteProc);
 
 
 implementation
 
-const SingleQuote:integer = $27;
-	
+//const SingleQuote:integer = $27;
 
-function getchar() : Integer;
-begin
-	try
-		getchar := tstr.ReadByte();
-	except
-		on E: EStreamError do
-			getchar := -1;
-	end;
-end;
-
-procedure MakeIdentifier();
-begin
-	{writeln('making identifier');}
-	yytext := '';
-	yytype := Identifier;
-	while isletterordigit(chr(cint)) and (cint <> -1) do begin
-		yytext := yytext + chr(cint);
-		{writeln('yyval~' + yyval); }
-		cint := getchar();
-	end;
-	{writeln('Identifier:' + yytext);}
-end;
-
-
-procedure MakeQuotedString();
-begin
-	{writeln('making quoted string');}
-	yytext := '';
-	yytype := Str;
-	while true do begin
-		cint := getchar();
-		if (cint = -1) or (cint = SingleQuote) then break;
-		yytext := yytext + chr(cint);
-	end;
-	cint := getchar();
-	{writeln('String:' + yytext);}
-end;
-
-procedure MakeUInt();
-begin
-	yytext := '';
-	yytype := UInt;
-	while isdigit(chr(cint)) and (cint <> -1) do begin
-		yytext := yytext + chr(cint);
-		cint := getchar();
-	end;
-end;
-
-procedure InitLexer(s:String);
-begin
-	tstr := TStringStream.Create(s);
-	cint := getchar();
-end;
-		
-
-function yylex(var the_yytext:string):TTokenType;
-{var 
-cint:integer; 
-tstr:TStringStream;
-yyval:String;}
-
-begin
-	the_yytext := '';
-	yytext := '';
-	yylex := Eof;
-	if cint = -1 then exit;
-	while iswhitespace(chr(cint)) do cint:= getchar();
-	if cint = -1 then exit;
-
-		
-	if isletter(chr(cint)) then 
-	begin
-			MakeIdentifier();
-	end
-	else if cint = SingleQuote then 
-	begin 
-		MakeQuotedString();
-	end
-	else if isdigit(chr(cint)) then
-	begin
-		MakeUInt();
-	end 
-	else
-	begin
-		{writeln('token skipping');}
-		yytype := Unknown;
-		cint := getchar();
-	end;
-
-	the_yytext := yytext;
-	yylex := yytype;
-	{writeln('cint=' + chr(cint)); }
-
-       
-	{writeln('. EOI');}
-
-end;
 
 
 
 procedure AddGluteProc(name:string; ptr:TGluteProc);
-{var
-	idx:integer;}
 begin
-	{writeln('AddGluteProc:' + name);}
 	procMap.Add(name, ptr);
-	{writeln(idx);}
-end;
-
-procedure say();
-begin
-	yylex(yytext);
-	writeln(yytext);
 end;
 
 
-function add1(vs:TVariantList):Variant;
-{$push}{$warn 5024 off}
-begin
-	yylex(yytext);
-	{writeln(yytype = UInt);}
-	add1 := 1 + StrToInt(yytext);
-	{writeln(add1);}
-end;
-{$pop}
+//function add1(vs:TVariantList):Variant;
+//{$push}{$warn 5024 off}
+//begin
+//	yylex(yytext);
+//	add1 := 1 + StrToInt(yytext);
+//end;
+//{$pop}
 
 function Pop(): Integer;
 begin
@@ -211,15 +96,6 @@ begin
         writeln('');
 end;
 
-procedure jesse();
-begin
-	writeln('Pinkman is not available right now');
-end;
-
-procedure yo();
-begin
-	writeln('Yo, yo, yo, 148-3369, representing the ABQ.');
-end;
 
 procedure xcept();
 begin
@@ -252,20 +128,6 @@ begin
 end;
 
 
-procedure MakeNum(var input:string; var pos:integer);
-var
-i:integer;
-begin
-        i := 0;
-        while(isdigit(input[pos])) do
-        begin
-                i := 10 * i + (ord(input[pos]) - ord('0'));
-                pos := pos +1;
-        end;
-
-        writeln('MakeNum:', i);
-
-end;
 
 procedure EvalWord(word: string);
 var
@@ -333,8 +195,6 @@ begin
                         writeln('');
 			if input = 'bye' then exit;
                         ParseLine(input);
-			//InitLexer(input);
-			//GluteEval();
 		except
 		on E: Exception do
 			DumpExceptionCallStack(E);
@@ -346,19 +206,12 @@ end;
 
 initialization
 begin
-	procMap := TProcMap.Create;
-        //IntStack := TStack.Create;
+        procMap := TProcMap.Create;
         IntStackSize := 0;
 
-	{AddGluteProc('add1', @add1);}
         AddGluteProc('.s',  @PrintStack);
         AddGluteProc('+',  @Plus);
-              {
-	AddGluteProc('say', @say);
-	AddGluteProc('jesse', @jesse);
-	AddGluteProc('xcept', @xcept);
-	AddGluteProc('yo', @yo);
-               }
+
 
 end;
 
