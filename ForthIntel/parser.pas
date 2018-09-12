c6b91bd980ccd68d7effb8f2844e708019361f94
Mark Carter <alt.mcarter@gmail.com>
2018-09-12 12:45:46 +0100

    Bring parser back into a working state

--- ForthIntel/parser.pas
+++ ForthIntel/parser.pas
@@ -10,7 +10,7 @@ uses
 	classes, {TStringStream }
  	character { IsWhiteSpace }
         //, contnrs
-	, sysutils
+        , sysutils
 	, variants
 	;
 
@@ -23,40 +23,22 @@ type
         //TWordPtr = ^TWord;
         TWord = record
           name:string;
-          link:^TWord; // link to the previous word in the dictionary
+          link:Integer; // link to the previous word in the dictionary
           codeptr:procedure();
-          dptr:Integer;
-          {*
-          case wtype:TWordType of
-               atomic: (ptr:procedure());
-               colonic: (HeapIndex:Integer);
-               integral: (int:Integer);
-               *}
+          dptr:Integer;  // dataspace
         end;
         TWordPtr = ^TWord;
 
-        //TCell = Integer;
-
         TTokenType = (Eof, Word, Int);
-        {*
-        Tyylval = record case vtype:TTokenType of
-          Eof: (finis:boolean);
-          Int: (i:Integer);
-          Word: (text:String);
-        end;
-        *}
 
-
-
-
-
-	//TProcMap = specialize TFPGMap<string, TGluteProc>;
-
+        TState = (compiling, interpreting);
 
 var
 	//cint:integer;
 	//tstr:TStringStream;
 	//yytype:TTokenType;
+
+        state:TState;
         tib:string; // terminal input buffer
         yypos:Integer; // a position within tib
         yylval_i:Integer;
@@ -66,62 +48,100 @@ var
 	//procMap:TProcMap;
         IntStack: array[1..200] of Integer;
         IntStackSize:Integer;
-        dict:^TWord;
+        dict:Integer;
         dataspace: array [1..30000] of byte; // data used by the dictionary
         dptr:Integer; // pointer the dataspace
         worddptr:Integer; // pointer to the dataspace for the current word
-        heap: array[1..MAX_HEAP] of ^TWord;
+        //heap: array[1..MAX_HEAP] of ^TWord;
+
+        hptr:Integer; // pointer into the heap
+        //heaptop:Integer;
         ip:Integer; // instruction pointer to the heap
+        heap:array[1..10000] of byte;
+        //heap1:Pointer;
 
 
 
 //procedure InitLexer(s:String);
 //function yylex(var the_yytext:String):TTokenType;
 procedure GluteRepl();
-procedure AddAtomic(name:string; ptr:TGluteProc);
+procedure AddAtomic(name:string; ptr:Pointer);
 procedure Push(val:integer);
+function yylex() : TTokenType;
+procedure yyparse();
 
 implementation
 
 //const SingleQuote:integer = $27;
 
 
-function lookup(name:string): TWordPtr;
+
+function DictName(ptr:Integer):string;
+var i:Integer;
 begin
-     //writeln('lookup called');
+        DictName := '';
+        for i:= 1 to heap[ptr+4] do DictName += char(heap[ptr+4+i]);
+end;
+function GetHeap32(pos:Integer):Integer;
+begin
+        Move(heap[pos], GetHeap32, 4);
+end;
+
+function lookup(name:string): Integer;
+begin
         lookup := dict;
-        while (lookup <> Nil) and (lookup^.name <> name) do lookup := lookup^.link;
-
-        if lookup = Nil then
+        while lookup <> 0 do
         begin
-                writeln('Word not found:', name);
-                exit;
+                if DictName(lookup) = name then exit;
+                lookup := GetHeap32(lookup);
         end;
-        worddptr := lookup^.dptr;
+
+        writeln('lookup failed for word:',name);
+
 end;
 
-procedure AddDictEntry(name:string; codeptr:TGluteProc);
-var NewWord:^TWord;
+procedure Heap32(val:Integer);
 begin
-        New(NewWord);
-        NewWord^.link := dict;
-        NewWord^.name := name;
-        NewWord^.codeptr := codeptr;
-        NewWord^.dptr := dptr;
-        dict := NewWord;
+        Move(val, heap[hptr], 4);
+        inc(hptr, 4);
+end;
+procedure HeapPointer(ptr:Pointer);
+begin
+        Move(ptr, heap[hptr], sizeof(Pointer));
+        inc(hptr, sizeof(Pointer));
+end;
+function GetHeapPointer(pos:Integer) : Pointer;
+begin
+        Move(heap[pos], GetHeapPointer, sizeof(Pointer));
+end;
+function WordCodeptr(d:Integer):Pointer;
+var offset:Integer;
+begin
+        offset := d  + 4 + heap[d+4] + 1;
+        writeln('WordCodeptr:offset:', offset);
+        WordCodeptr := GetHeapPointer(offset);
+        writeln('WordCodeptr:',  Int64(WordCodeptr));
+
 end;
 
-procedure AddAtomic(name:string; ptr:TGluteProc);
-//var        NewWord:^TWord;
+procedure AddAtomic(name:string; ptr:Pointer);
+var
+        tmp, i:Integer;
 begin
-        {*
-        New(NewWord);
-        NewWord^.link := dict;
-        NewWord^.name := name;
-        NewWord^.ptr := ptr;
-        dict := NewWord;
-        *}
-        AddDictEntry(name, ptr);
+        tmp := hptr; // this will become the new top of the dictionary
+        Heap32(dict);  // link
+        heap[hptr] := length(name); inc(hptr); // name length
+
+        // write out the name
+        for i := 1 to length(name) do
+        begin
+                heap[hptr] := ord(name[i]);
+                inc(hptr);
+        end;
+
+        HeapPointer(ptr); // codeptr
+        dict := tmp;
+
 end;
 
 procedure PushInteger();
@@ -132,7 +152,6 @@ begin
         for i:= 0 to 3 do bytes[i] := dataspace[worddptr + i];
         val := 0; // supress compilation warning
         Move(bytes, val, 4);
-        //Push(val);
 end;
 
 procedure PushDataByte(b:byte);
@@ -147,20 +166,14 @@ var
         i: Integer;
 begin
         writeln('EvalInteger called:', val);
+        {*
         AddDictEntry('', @PushInteger);
         bytes[0] := 0; // suppress warning about not being initialised
         Move(val, bytes, 4);
         for i := 0 to 3 do  PushDataByte(bytes[i]);
+        *}
         Push(val);
 end;
-
-//function add1(vs:TVariantList):Variant;
-//{$push}{$warn 5024 off}
-//begin
-//	yylex(yytext);
-//	add1 := 1 + StrToInt(yytext);
-//end;
-//{$pop}
 
 function Pop(): Integer;
 begin
@@ -195,7 +208,6 @@ begin
         writeln('PrintStack called');
         for i := 1 to IntStackSize do
                 write(IntStack[i], ' ');
-        writeln('');
 end;
 
 procedure Dot();
@@ -203,12 +215,58 @@ begin
         write(Pop(), ' ');
 end;
 
+procedure Dup();
+var i:Integer;
+begin
+        i := Pop();
+        Push(i);
+        Push(i);
+end;
 procedure xcept();
 begin
 	Raise exception.create('Xcept exception test');
 end;
 
-			
+procedure Noop;
+begin
+        // don't do anything
+end;
+
+procedure CreateWith(proc:TGluteProc);
+begin
+     yylex();
+     //AddDictEntry(yytext, proc);
+     yyparse();
+end;
+
+procedure Create();
+begin
+     CreateWith(@Noop);
+        //yylex();
+        //AddDictEntry(yytext, @Noop);
+        //yyparse();
+end;
+
+procedure DoCol();
+begin
+     writeln('DoCol TODO');
+end;
+
+procedure Colon();
+begin
+        state := compiling;
+        CreateWith(@DoCol);
+     //writeln('Colon TODO');
+
+
+end;
+
+procedure SemiColon();
+begin
+     state := interpreting;
+        writeln('Semicolon TODO');
+end;
+
 procedure DumpExceptionCallStack(E: Exception);
 var
 	I: Integer;
@@ -235,16 +293,41 @@ end;
 
 
 function IsInt() :Boolean;
+var pos, sgn, len, i:Integer;
 begin
-         try
-                 IsInt := true;
-                 yylval_i := StrToInt(yytext);
-                 //yylex := Int;
-                 //IntStackSize := IntStackSize +1;
-                 //IntStack[IntStackSize] := i;
-         except
-         On E: EConvertError do IsInt := false;
-         end;
+        yylval_i := 0;
+        IsInt := true;
+        pos := 1;
+        sgn := 1;
+        len := length(yytext);
+
+        // deal with potential negative
+        if yytext[1] = '-' then
+        begin
+                sgn := -1;
+                pos := pos +1;
+                if pos> len then
+                begin
+                        IsInt := false;
+                        exit;
+                end;
+        end;
+
+        for i:= pos to len do
+                begin
+                        if isdigit(yytext[i]) then
+                        begin
+                                yylval_i := 10 * yylval_i + (ord(yytext[i]) - ord('0'));
+                        end
+                        else
+                        begin
+                                IsInt := false;
+                                exit;
+                                end;
+                end;
+
+        yylval_i := sgn * yylval_i;
+
 end;
 
 function yylex() : TTokenType;
@@ -283,15 +366,31 @@ begin
         end;
 end;
 
-procedure EvalWord(name:string);
-var wptr:TWordptr; ptr: TGluteProc;
+
+{*
+procedure CallCodePtr(fn:TGluteProc);
 begin
-     wptr := lookup(name);
-     if(wptr = Nil) then exit;
-     writeln('word found');
-     ptr := wptr^.codeptr;
-     worddptr := wptr^.dptr;
-     ptr();
+        fn();
+end;
+
+*}
+
+procedure EvalWord(name:string);
+var wptr:Integer; ptr:Pointer; ptr1:TGluteProc;
+begin
+        wptr := lookup(name);
+        if wptr = 0 then
+        begin
+                writeln('Word unfound:', name);
+                exit;
+        end;
+
+        writeln('word found');
+        ptr := WordCodeptr(wptr);
+        writeln('word found:', Integer(ptr));
+        //CallCodePtr(ptr);
+        ptr1 := TGluteProc(ptr);
+        ptr1();
 end;
 
 procedure EvalToken(yytype:TTokenType);
@@ -306,25 +405,94 @@ begin
         end;
 
 end;
+procedure yyparse();
+var yystype:TTokenType;
+begin
+     yystype := yylex();
+     EvalToken(yystype);
+//     heap[heaptop] := dict;
+  //   heaptop := heaptop + 1;
+end;
+
+procedure Branch();
+var i:Integer; yytype:TTokenType;
+begin
+        yytype := yylex();
+        EvalToken(yytype);
+end;
+procedure qBranch();
+var i:Integer; yytype:TTokenType;
+begin
+     yytype := yylex();
+     EvalToken(yytype);
+end;
+procedure words();
+var d:Integer;
+begin
+     d := dict;
+     {*
+     while (d <> 0) do
+     begin
+             write(d^.name, ' ');
+             d := d^.link;
+     end;
+     *}
+     writeln();
+end;
+
+procedure Dump();
+{
+var
+        i:Integer;
+        w, d:TWordPtr;
+        codeptr:TGluteProc;
+        }
+begin
+     {*
+     writeln('Heap:');
+     for i:= 1 to heaptop-1 do
+     begin
+             write('Heap:', i, ' ');
+             w := heap[i];
+
+             // reverse lookup the codeptr
+             codeptr := w^.codeptr;
+             d := dict;
+             while d^.codeptr <> codeptr do d := d^.link;
+             writeln(w^.name, ' ', d^.name);
+     end;
+     writeln('dataspace:');
+     for i := 1 to dptr-1 do
+     begin
+             writeln(i, '   ' , dataspace[i]);
+     end;
+     words();
+     *}
+end;
+
+procedure info();
+begin
+     writeln('Sizeof:Integer: ', sizeof(Integer));
+     writeln('Sizeof:Int64:   ', sizeof(Int64));
+     writeln('Sizeof:Pointer: ', sizeof(Pointer));
+end;
+
 procedure GluteRepl();
-var yytype:TTokenType;
+//var yytype:TTokenType;
 //input:string;
 begin
-
-	while true do begin
-		try
-		       	write(':');
-			//readln(input);
-                        yytype := yylex();
-                        writeln('');
+     info();
+     while true do begin
+             try
+                        write(':');
+                        yyparse();
 			if yytext = 'bye' then exit;
-                        //ParseLine(input);
-                        EvalToken(yytype);
+                        //Dump();
 		except
 		on E: Exception do
 			DumpExceptionCallStack(E);
 		end;
-	end;
+     end;
 
 end;
 
@@ -333,17 +501,30 @@ initialization
 begin
         //procMap := TProcMap.Create;
         IntStackSize := 0;
-        dict := Nil;
+        dict := 0;
         ip := 1;
         tib := '';
         yytext := '';
         yypos := 1;
         dptr := 1;
+        //heaptop := 1;
+        hptr := 1;
+        state := interpreting;
+        //heap1 := malloc(10000);
+
         AddAtomic('.s',  @PrintStack);
         AddAtomic('+',  @Plus);
         AddAtomic('.', @Dot);
-
-
+        AddAtomic('dup', @Dup);
+        AddAtomic('branch', @Branch);
+        AddAtomic('branch?', @qBranch);
+        AddAtomic('dump', @Dump);
+        AddAtomic(':', @Colon);
+        AddAtomic(';', @Semicolon);
+        AddAtomic('words', @Words);
+        AddAtomic('create', @Create);
+        writeln('Init:@PrintStack:',  Int64(@PrintStack));
+        //lookup('create');
 end;
 
 end.
