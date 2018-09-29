eae9a9f9e621d0825ca34ed7ee443ef909b405a8
markfirmware <markfirmware@users.noreply.github.com>
2018-09-29 12:19:35 -0400

    wip

--- ultiboapi.pas
+++ ultiboapi.pas
@@ -14,14 +14,19 @@ begin
  Result:=X <> 0;
 end;
 
-procedure P_Ultibo_Uses_UltiboApi_StartSerialLogging;
+procedure P_Ultibo_Uses_Platform_ActivityLedEnable;
 begin
- LOGGING_INCLUDE_COUNTER:=False;
- LOGGING_INCLUDE_TICKCOUNT:=True;
- SERIAL_REGISTER_LOGGING:=True;
- SerialLoggingDeviceAdd(SerialDeviceGetDefault);
- SERIAL_REGISTER_LOGGING:=False;
- LoggingDeviceSetDefault(LoggingDeviceFindByType(LOGGING_TYPE_SERIAL));
+ ActivityLedEnable;
+end;
+
+procedure P_Ultibo_Uses_Platform_ActivityLedOff;
+begin
+ ActivityLedOff;
+end;
+
+procedure P_Ultibo_Uses_Platform_ActivityLedOn;
+begin
+ ActivityLedOn;
 end;
 
 procedure P_Ultibo_Uses_Console_ConsoleWindowGetDefault;
@@ -42,11 +47,30 @@ begin
  Push(ConsoleWindowClearEx(Handle,X1,Y1,X2,Y2,IntToBool(Cursor)));
 end;
 
+procedure P_Ultibo_Uses_Ultibo_Sleep;
+begin
+ Sleep(Pop);
+end;
+
+procedure P_Ultibo_Uses_UltiboApi_StartSerialLogging;
+begin
+ LOGGING_INCLUDE_COUNTER:=False;
+ LOGGING_INCLUDE_TICKCOUNT:=True;
+ SERIAL_REGISTER_LOGGING:=True;
+ SerialLoggingDeviceAdd(SerialDeviceGetDefault);
+ SERIAL_REGISTER_LOGGING:=False;
+ LoggingDeviceSetDefault(LoggingDeviceFindByType(LOGGING_TYPE_SERIAL));
+end;
+
 procedure UltiboApiAddPrimitives;
 begin
- AddPrim(0, 'Ultibo.Uses.UltiboApi.StartSerialLogging', @P_Ultibo_Uses_UltiboApi_StartSerialLogging);
- AddPrim(0, 'Ultibo.Uses.Console.ConsoleWindowGetDefault', @P_Ultibo_Uses_Console_ConsoleWindowGetDefault);
  AddPrim(0, 'Ultibo.Uses.Console.ConsoleWindowClearEx', @P_Ultibo_Uses_Console_ConsoleWindowClearEx);
+ AddPrim(0, 'Ultibo.Uses.Console.ConsoleWindowGetDefault', @P_Ultibo_Uses_Console_ConsoleWindowGetDefault);
+ AddPrim(0, 'Ultibo.Uses.Platform.ActivityLedEnable', @P_Ultibo_Uses_Platform_ActivityLedEnable);
+ AddPrim(0, 'Ultibo.Uses.Platform.ActivityLedOff', @P_Ultibo_Uses_Platform_ActivityLedOff);
+ AddPrim(0, 'Ultibo.Uses.Platform.ActivityLedOn', @P_Ultibo_Uses_Platform_ActivityLedOn);
+ AddPrim(0, 'Ultibo.Uses.Ultibo.Sleep', @P_Ultibo_Uses_Ultibo_Sleep);
+ AddPrim(0, 'Ultibo.Uses.UltiboApi.StartSerialLogging', @P_Ultibo_Uses_UltiboApi_StartSerialLogging);
 end;
 
 end.
